beda2e90c284e339effe0a6e5910ef828dc4594d
Kenny Billiau <Kenny.billiau@scilifelab.se>
2018-10-02 22:20:49 +0200

    Rm err trap
    That is caught in crontab.
    
    Make sure the project dir is a dir

--- scripts/checkfornewproject.sh
+++ scripts/checkfornewproject.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-set -eu
 shopt -s nullglob
 shopt -s expand_aliases
 source ~/.bashrc
+set -eu
 
 ########
 # VARS #
 ########
 
-MAILTO=clinical-demux@scilifelab.se
-MAILTO_ERR=clinical-demux@scilifelab.se
 INDIR=${1?'please provide dir holding all projects'}
 
 #############
@@ -22,20 +20,16 @@ log() {
     echo "[$NOW] $@"
 }
 
-failed() {
-    echo "Error starting ${PROJECT}: $(caller)" | mail -s "ERROR starting ${PROJECT}" ${MAILTO_ERR}
-}
-trap failed ERR
-
 ########
 # MAIN #
 ########
 
 for PROJECT_DIR in ${INDIR}/*; do
     PROJECT=$(basename $PROJECT_DIR)
-    if [[ ! -f ${PROJECT_DIR}/analysis_started.txt ]]; then
+    if [[ -d ${PROJECT_DIR} && ! -f ${PROJECT_DIR}/analysis_started.txt ]]; then
         log "${PROJECT}: starting!"
         mwgs project ${PROJECT_DIR}
+        date > ${PROJECT_DIR}/analysis_started.txt
     else
         log "${PROJECT}: already started"
     fi
